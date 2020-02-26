91dceb7cf6d0a7058ebeabf8c4e071864c206e95
zhongyi <1454827785@qq.com>
2020-02-26 21:50:10 +0800

    second

--- test1.sh
+++ test1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone -b master https://github.com/flyzy2005/ss-fly;
-echo -e "zy123\n\n\n\n" | ss-fly/ss-fly.sh -ssr;
-ss-fly/ss-fly.sh -bbr;
+read -p "请输入您SSR的密码：" input;echo -e "$input\n\n\n\n\n" | ss-fly/ss-fly.sh -ssr;
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh;
+echo '开始重启，请稍候';
 reboot;
