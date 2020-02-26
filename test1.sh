790884e56c31529c8b4ec1bb7dbd9ac1ac6074d6
zhongyi <1454827785@qq.com>
2020-02-26 22:17:52 +0800

    second2

--- test1.sh
+++ test1.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 git clone -b master https://github.com/flyzy2005/ss-fly;
 read -p "请输入您SSR的密码：" input;echo -e "$input\n\n\n\n\n" | ss-fly/ss-fly.sh -ssr;
-wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh;
-echo '开始重启，请稍候';
-reboot;
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && grep 'Your Server IP' shadowsocksR.log -A 6 &&./bbr.sh;
